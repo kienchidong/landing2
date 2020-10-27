9137e664b4f6061203c25fbabbaf63476440903a
kienchidong <kienchidong@gmail.com>
2020-10-27 19:08:45 +0700

    abc

---
+++ src/components/landing.vue
@@ -0,0 +1,240 @@
+<template>
+    <div class="root">
+        <section class="banner">
+            <div class="header-up">
+                <img src="../assets/images/untitled_2_03.png"/>
+            </div>
+            <div class="banner-text row">
+
+              <!--  <a class="btn btn-store" href="#">
+                        <img src="../assets/images/get-it-on-google-play.png"/>
+                    </a>
+                    <a class="btn btn-store" href="#">
+                        <img src="../assets/images/appstore.png"/>
+                    </a>-->
+
+
+                    <img src="../assets/images/Untitled-2-02@2x.png" width="100%"/>
+
+                    <img src="../assets/images/header-text.png" width="100%"/>
+
+            </div>
+        </section>
+        <section class="timing ">
+            <b>Thời gian quyên góp</b>
+            <p>
+                Đợt 1: 28-10 đến 11-11 | Đợt 2: 12-11 đến 26-11
+            </p>
+        </section>
+        <section class="content">
+            <div class="container">
+                <div class="group_83">
+                    <img src="../assets/images/content/Group_168.png" alt="">
+                    <a href="#" class="btn btn-light btn-download" target="_blank">Tải ngay</a>
+                </div>
+                <div class="outer rotate-left">
+                    <div class="inner rotate">
+                        #Hướng về miền trung
+                    </div>
+                </div>
+                <div class="outer rotate-right">
+                    <div class="inner rotate">
+                        #Beberia
+                    </div>
+                </div>
+                <div class="group_83">
+                    <div id="multi-item-example" class="carousel slide carousel-multi-item" data-ride="carousel">
+
+                        <!--Controls-->
+                        <div class="controls-top">
+                            <a class="btn-floating" href="#multi-item-example" data-slide="prev"><i
+                                    class="fa fa-chevron-left"></i></a>
+                            <a class="btn-floating" href="#multi-item-example" data-slide="next"><i
+                                    class="fa fa-chevron-right"></i></a>
+                        </div>
+                        <!--/.Controls-->
+
+                        <!--Indicators-->
+                        <!-- <ol class="carousel-indicators">
+                             <li data-target="#multi-item-example" data-slide-to="0" class="active"></li>
+                             <li data-target="#multi-item-example" data-slide-to="1"></li>
+                             <li data-target="#multi-item-example" data-slide-to="2"></li>
+                         </ol>-->
+                        <!--/.Indicators-->
+
+                        <!--Slides-->
+                        <div class="carousel-inner" role="listbox">
+
+                            <!--First slide-->
+                            <div class="carousel-item active">
+
+                                <div class="row">
+
+                                    <div class="col-md-4 clearfix d-none d-md-block">
+                                        <img src="../assets/images/slider/Rectangle_2898_di@2x.png" width="100%"/>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <img src="../assets/images/slider/Mask_Group_26@2x.png" width="100%"/>
+                                        <h4>Bước 1</h4>
+                                        <p>Cài đặt ứng dụng trên Store</p>
+                                    </div>
+                                    <div class="col-md-4 clearfix d-none d-md-block">
+                                        <img src="../assets/images/slider/Rectangle_2898@2x.png" width="100%"/>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+                            <!--/.First slide-->
+
+                            <!--Second slide-->
+                            <div class="carousel-item">
+
+                                <div class="row">
+
+                                    <div class="col-md-4 clearfix d-none d-md-block">
+                                        <img src="../assets/images/slider/Mask_Group_26@2x.png" width="100%"/>
+
+                                    </div>
+                                    <div class="col-md-4 ">
+
+                                        <img src="../assets/images/slider/Rectangle_2898@2x.png" width="100%"/>
+                                        <h4>Bước 2</h4>
+                                        <p>Đăng nhập</p>
+                                    </div>
+                                    <div class="col-md-4 clearfix d-none d-md-block">
+                                        <img src="../assets/images/slider/Rectangle_2898_di@2x.png" width="100%"/>
+
+                                    </div>
+                                </div>
+
+                            </div>
+                            <!--/.Second slide-->
+
+                            <!--Third slide-->
+                            <div class="carousel-item">
+
+                                <div class="row">
+                                    <div class="col-md-4 clearfix d-none d-md-block ">
+
+                                        <img src="../assets/images/slider/Rectangle_2898@2x.png" width="100%"/>
+
+                                    </div>
+                                    <div class="col-md-4 ">
+                                        <img src="../assets/images/slider/Rectangle_2898_di@2x.png" width="100%"/>
+                                        <h4>Bước 3</h4>
+                                        <p>Gửi lời chào đến các mom kèm hastag<br> #Huongvemientrung</p>
+                                    </div>
+                                    <div class="col-md-4 clearfix d-none d-md-block ">
+                                        <img src="../assets/images/slider/Mask_Group_26@2x.png" width="100%"/>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <!--/.Third slide-->
+
+                        </div>
+                        <!--/.Slides-->
+
+                    </div>
+
+                </div>
+            </div>
+            <div style="background-color: #F5ADBE">
+                <div class="main-content">
+                    <!--  <div class="content-member">
+                          <a href="#">
+                              <img src="../assets/images/content/group_53.svg" alt="">
+                          </a>
+                      </div>-->
+                    <table class="content-member">
+                        <tr>
+                            <td>
+                                <img src="../assets/images/content/group_70.svg"/>
+                            </td>
+                            <td>
+                                <h3>9.999</h3>
+                                <span>Lượt truy cập</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <img src="../assets/images/content/group_71.svg"/>
+                            </td>
+                            <td>
+                                <h3>5.000</h3>
+                                <span>Lượt tải</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <img src="../assets/images/content/group_69.svg"/>
+                            </td>
+                            <td>
+                                <h3>50.000.000</h3>
+                                <span>Số Tiền Quyên góp</span>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <section class="footer">
+                    <a href="https://www.facebook.com/beberia.mebe" target="_blank">
+                        <img src="../assets/images/footer/Mask_Group_3.png" width="5%"/>
+                        Fanpage Beberia - Mẹ con mình
+                    </a>
+                    <a href="https://www.youtube.com/channel/UCJNPxZEUSbVECdDbWCMfw1A" target="_blank">
+                        <img src="../assets/images/footer/Mask_Group_2.png" width="5%"/>
+                        Beberia - Mẹ và bé
+                    </a>
+                </section>
+                <div class="img-footer">
+                    <img src="../assets/images/footer/group_75.png" width="100%"/>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "landing",
+        mounted() {
+            var {google} = require("googleapis");
+
+            // Load the service account key JSON file.
+            var serviceAccount = require("../assets/beberia-adminsdk.json");
+
+            console.log(serviceAccount)
+            // Specify the required scope.
+            var scopes = [
+                "https://www.googleapis.com/auth/firebase"
+            ];
+
+            // Authenticate a JWT client with the service account.
+            var jwtClient = new google.auth.JWT(
+                serviceAccount.client_email,
+                null,
+                serviceAccount.private_key,
+                scopes
+            );
+
+            // Use the JWT client to generate an access token.
+            jwtClient.authorize(function (error, tokens) {
+                if (error) {
+                    console.log("Error making request to generate access token:", error);
+                } else if (tokens.access_token === null) {
+                    console.log("Provided service account does not have permission to generate access tokens");
+                } else {
+                    var accessToken = tokens.access_token;
+                    console.log(accessToken)
+                    // Include the access token in the Authorization header.
+                }
+            });
+        }
+    }
+</script>
+
+<style scoped>
+    @import "../assets/style/style.css";
+</style>
